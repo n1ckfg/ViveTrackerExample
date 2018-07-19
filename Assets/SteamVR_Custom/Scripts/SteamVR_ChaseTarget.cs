d5a1021bd85d3b8ed2d16b3d58434bde084f5935
n1ckfg <nick@fox-gieg.com>
2018-07-19 01:05:21 -0400

    refactor

---
+++ Assets/SteamVR_Custom/Scripts/SteamVR_ChaseTarget.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteamVR_ChaseTarget : MonoBehaviour {
+
+    public Transform target;
+    public bool doLerp = true;
+    public float lerpVal = 0.5f;
+
+    private bool firstRun = true;
+
+    private void Update() {
+        if (doLerp) {
+            if (firstRun) {
+                matchPosRot();
+                firstRun = false;
+            } else {
+                lerpPosRot();
+            }
+         } else {
+            matchPosRot();
+        }
+    }
+
+    private void lerpPosRot() {
+        transform.position = Vector3.Lerp(transform.position, target.position, lerpVal);
+        transform.rotation = Quaternion.Lerp(transform.rotation, target.rotation, lerpVal);
+    }
+
+    private void matchPosRot() {
+        transform.position = target.position;
+        transform.rotation = target.rotation;
+    }
+
+}
